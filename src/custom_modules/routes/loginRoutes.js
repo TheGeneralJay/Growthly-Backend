b69997bffb3d1f8d77ff4b677df742208b8b4adf
Amanda <amandagurns@hotmail.ca>
2025-01-29 11:15:22 -0500

    Send user upon login.

--- src/custom_modules/routes/loginRoutes.js
+++ src/custom_modules/routes/loginRoutes.js
@@ -13,19 +13,19 @@ router.post("/", async (req, res) => {
   const existingUser = req.body;
 
   // Find the user by email or by username.
-  let dbResponse = await db.userModel.findOne({
+  let dbUser = await db.userModel.findOne({
     email: existingUser.email_or_username,
   });
 
-  if (!dbResponse) {
-    dbResponse = await db.userModel.findOne({
+  if (!dbUser) {
+    dbUser = await db.userModel.findOne({
       username: existingUser.email_or_username,
     });
   }
 
   // If no matching user exists, notify the user.
   try {
-    if (!dbResponse) {
+    if (!dbUser) {
       throw new Error();
     }
   } catch (err) {
@@ -35,15 +35,15 @@ router.post("/", async (req, res) => {
 
   // Test the entered plaintext password against the hashed version in the DB.
   try {
-    const isMatch = await dbResponse.comparePassword(existingUser.password);
+    const isMatch = await dbUser.comparePassword(existingUser.password);
 
     // If the passwords do not match, throw error.
     if (!isMatch) {
       throw new Error();
     }
 
-    // If we get here, send the successful login message.
-    res.status(200).json(`Login successful, welcome ${dbResponse.first_name}!`);
+    // If we get here, send the user that logged in.
+    res.status(200).json(dbUser);
   } catch (err) {
     res
       .status(ERR.INCORRECT_PASSWORD_ERROR.status)
